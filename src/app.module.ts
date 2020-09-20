bbe800013ecb7d3dcc7a0a9495cdc3e4e09bfa29
ProgrammerInc <programmer.incorporated@gmail.com>
2020-09-19 21:11:10 -0500

    Add Support for Express Compression, CORS, Csurf, Helmet, Rate Limiting with Configuration Variables.

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,6 +17,13 @@ import { PrismaModule } from './prisma/prisma.module';
           .valid('development', 'production', 'test', 'provision')
           .default('development'),
         PORT: Joi.number().default(3000),
+        ENABLE_COMPRESSION: Joi.boolean().default(false),
+        ENABLE_CORS: Joi.boolean().default(false),
+        ENABLE_CSURF: Joi.boolean().default(false),
+        ENABLE_HELMET: Joi.boolean().default(false),
+        ENABLE_RATELIMIT: Joi.boolean().default(false),
+        RATELIMIT_WINDOW: Joi.number().default(15),
+        RATELIMIT_MAX: Joi.number().default(100),
         DATABASE_URL: Joi.string().default('postgresql://postgres@localhost:5432/programmer-news?schema=public'),
         REDIS_URL: Joi.string().default('redis://localhost:6379'),
       }),
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import * as compression from 'compression';
+import * as csurf from 'csurf';
 import * as dotenv from 'dotenv';
+import * as rateLimit from 'express-rate-limit';
+import * as helmet from 'helmet';
 import { AppModule } from './app.module';
 
 dotenv.config();
@@ -8,7 +12,42 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const port = configService.get('PORT');
+  const port = configService.get<number>('PORT');
+
+  const enableCompression = configService.get<boolean>('ENABLE_COMPRESSION');
+
+  if (enableCompression) {
+    app.use(compression());
+  }
+
+  const enableCors = configService.get<boolean>('ENABLE_CORS');
+
+  if (enableCors) {
+    app.enableCors();
+  }
+
+  const enableCsurf = configService.get<boolean>('ENABLE_CSURF');
+
+  if (enableCsurf) {
+    app.use(csurf());
+  }
+
+  const enableHelmet = configService.get<boolean>('ENABLE_HELMET');
+
+  if (enableHelmet) {
+    app.use(helmet());
+  }
+
+  const enableRateLimit = configService.get<boolean>('ENABLE_RATELIMIT');
+
+  if (enableRateLimit) {
+    app.use(
+      rateLimit({
+        windowMs: configService.get<number>('RATELIMIT_WINDOW') * 60 * 1000,
+        max: configService.get<number>('RATELIMIT_MAX'),
+      }),
+    );
+  }
 
   await app.listen(port);
 }
